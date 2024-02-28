4477ef1af6381057a5cf3958c5d3ff7612b41065
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-02-28 18:29:47 +0000

    Completed assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,6 @@ class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
 
-    render template: "movies/new"
   end
 
   def index
@@ -16,7 +15,7 @@ class MoviesController < ApplicationController
       end
 
       format.html do
-        render( template: "movies/index" )
+        render "movies/index" 
       end
     end
   end
@@ -28,7 +27,6 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render( template: "movies/show" )
   end
 
   def create
@@ -38,11 +36,12 @@ class MoviesController < ApplicationController
 
     if @the_movie.valid?
       @the_movie.save
-      redirect_to("/movies", notice: "Movie created successfully." )
+      redirect_to movies_url, notice: "Movie created successfully." 
     else
       render template: "movies/new"
     end
-  end
+
+  end # Of method.
 
   def edit
     the_id = params.fetch(:id)
@@ -51,7 +50,6 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render( template: "movies/edit" )
   end
 
   def update
@@ -63,9 +61,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", notice: "Movie updated successfully." )
+      redirect_to movies_url, notice: "Movie updated successfully." 
     else
-      redirect_to("/movies/#{the_movie.id}", alert: "Movie failed to update successfully." )
+      redirect_to movies_url, alert: "Movie failed to update successfully." 
     end
   end
 
@@ -75,6 +73,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to("/movies", notice: "Movie deleted successfully." )
+    redirect_to movies_url, notice: "Movie deleted successfully." 
   end
 end
